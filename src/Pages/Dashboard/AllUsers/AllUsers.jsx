6484c3c625a7bbc90a4c9a39b5b1e118f77d41f9
Masrafi404 <mdmasrafi902@gmail.com>
2023-06-10 07:17:46 +0600

    user delete

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -10,7 +10,32 @@ const AllUsers = () => {
     })
 
     const handlerDelete = user => {
-
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/users/${user._id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire(
+                                'Deleted!',
+                                `Your ${user.name} user has been deleted.`,
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
     }
 
     const handlerMakeAdmin = user => {
