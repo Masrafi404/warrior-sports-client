15ae6cf90d7f9774e7c12b8cbc3e906acb66b176
Masrafi404 <mdmasrafi902@gmail.com>
2023-06-11 21:35:10 +0600

    jwt

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -4,10 +4,16 @@ import { AuthContext } from '../Pages/Shared/Provider/AuthProvider';
 const useCart = () => {
     const { user } = useContext(AuthContext);
 
+    const token = localStorage.getItem('access-token')
+
     const { isLoading, refetch, data: cart = [] } = useQuery({
         queryKey: ['cart', user?.email],
         queryFn: async () => {
-            const response = await fetch(`http://localhost:5000/carts?email=${user.email}`)
+            const response = await fetch(`http://localhost:5000/carts?email=${user.email}`, {
+                headers: {
+                    authorization: `bearer ${token}`
+                }
+            })
             return response.json()
         },
     })
